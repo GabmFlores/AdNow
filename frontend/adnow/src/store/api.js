b6780954f348effe5b7b6ae987b6d915d976340e
Gabriel Mari Flores <renshuuuu@Gabriels-MacBook-Air.local>
2024-12-16 05:51:48 +0800

    brute api.js

--- frontend/adnow/src/store/api.js
+++ frontend/adnow/src/store/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-// Conditionally set the API URL based on the environment
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? import.meta.env.VITE_API_URL
-      : "http://localhost:5002/api", // Default to local URL in development
+  baseURL: "https://adnow.onrender.com/api", // Directly set the production URL
   withCredentials: true, // Allow cookies and session data
 });
 
