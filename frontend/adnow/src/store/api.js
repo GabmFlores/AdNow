58bc0cdb69129333eda22eeb2df1c3558fa7b3d2
Gabriel Mari Flores <renshuuuu@Gabriels-MacBook-Air.local>
2024-12-16 06:20:56 +0800

    will change .env

--- frontend/adnow/src/store/api.js
+++ frontend/adnow/src/store/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// Conditionally set the API URL based on the environment
 const api = axios.create({
-  baseURL: "https://adnow.onrender.com/api", // Directly set the production URL
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? import.meta.env.VITE_API_URL
+      : "http://localhost:5002/api", // Default to local URL in development
   withCredentials: true, // Allow cookies and session data
 });
 
